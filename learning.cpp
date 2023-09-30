c27f97c231dfc692cedf26867741af4a7cefcf1e
Sarab160 <142167410+Sarab160@users.noreply.github.com>
2023-09-30 20:31:15 +0500

    first file

---
+++ learning.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // cout<<"sarab cheema";
+    int num = 3;
+    float num2 = 3.343;
+    char name = 87;
+    int number,num4,red;
+    cin>>number;
+    cin>>num4;
+    cout<<number+num4;
+    cout<<name;
+    return 0;
+}
+
+
