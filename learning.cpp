64e40e5ed5417f49c1b575fe67c69ca5b04159cc
Sarab160 <142167410+Sarab160@users.noreply.github.com>
2023-10-01 23:04:14 +0500

    file updated

--- learning.cpp
+++ learning.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 int main(){
-
-    // cout<<"sarab cheema";
+   
+    cout<<"sarab cheema";
     int num = 3;
     float num2 = 3.343;
     char name = 87;
@@ -10,8 +10,43 @@ int main(){
     cin>>number;
     cin>>num4;
     cout<<number+num4;
-    cout<<name;
+     cout<<name;
+    // return 0;
+     cout<<"**write your calculator program** \n";
+    cout<<"Enter the numbers and solve the logics \n";
+
+    float number1,number2;
+
+    cout<<" \n ENTER YOUR FIRST NUMBER :";
+    cin>>number1;
+
+    cout<<" \n ENTER YOUR SECOND NUMBER :";
+    cin>>number2;
+
+   float sum,div,sub,mult;
+   sum = number1+number2;
+   sub = number1-number2;
+   div = number1 / number2;
+
+   mult = number1 * number2;
+
+   cout<<"\n sum = "<<sum;
+
+   cout<<" \n divison = "<<div;
+
+   cout<< " \n subtraction ="<<sub;
+
+   cout<<" \n multiplication ="<<mult; 
+
+cout<<"Hello World\tto you";
+cout<<"\\Hello World";
+cout<<"Hello World\rto you";
+cout<<\"Hello World to you";
+cout<<"Hello World to you\"";
+
     return 0;
 }
+
+
 
 
