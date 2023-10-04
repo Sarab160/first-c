77d5f4ebdad9bbd6f30539c5d0cd32bc218c0983
Sarab160 <142167410+Sarab160@users.noreply.github.com>
2023-10-04 21:18:08 +0500

    file updated

---
+++ class.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int number=0;
+    while(number<10){
+    cout<<number<<endl;
+    number++;
+    }
+    for (number<10;number++;){
+       cout<<number<<endl;
+    }
+    system("pause");
+   
+}
