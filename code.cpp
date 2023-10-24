5f9c7010ad7f8235fefb45857de01b51d10d5192
Sarab160 <142167410+Sarab160@users.noreply.github.com>
2023-10-24 23:13:17 +0500

    file added

---
+++ code.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std; 
+int main(){
+    // float num = 6.34;
+    // int num2 = static_cast <int>(num);
+    // cout<<"the value of num = "<<num2<<endl;
+    // float num1 ;
+    // float num3 = 876.34;
+    // num1=int(num3);
+    // cout<<"value of num"<<num1<<endl;
+    // float number = 23.65;
+    // float number2 ;
+    // number2=number;
+    // cout<<"the value of number 2 = "<<number2<<endl;
+    int number4 = 23;
+    char number5 = static_cast<char>(number4);
+    cout<<"the new value is = "<<number5;
+
+
+}
+
--- function.cpp
+++ function.cpp
@@ -24,19 +24,20 @@ int function(int num3,int num4){
   return result1;
 }
 int main(){
-    int callfunction = function(3,4);
-    cout<<callfunction<<endl;
+    // int callfunction = function(3,4);
+    // cout<<callfunction<<endl;
 
-    // while(true){
-    // sum();};
-    int a = 3;
-    int *point = &a;
-    cout<<point<<endl; 
-    string name,father;
-    cout<<"enter your name\n";
-    cin>>name;
-    cout<<"enter yur father name\n";
-    cin>>father;
-    cout<<"your data is here\n";
-    cout<<name <<" son of "<<father;
+    // // while(true){
+    // // sum();};
+    // int a = 3;
+    // int *point = &a;
+    // cout<<point<<endl; 
+    // string name,father;
+    // cout<<"enter your name\n";
+    // cin>>name;
+    // cout<<"enter yur father name\n";
+    // cin>>father;
+    // cout<<"your data is here\n";
+    //cout<<name <<" son of "<<father;
+     
 }
